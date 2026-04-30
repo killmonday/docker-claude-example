354b2403a40f9f896cc4a103ea8c6b6d39dc6433
killmonday <59520081+killmonday@users.noreply.github.com>
2026-04-30 14:18:17 +0800

    Fix permissions and create directory for vscode user
    换行修正

--- env_init.sh
+++ env_init.sh
@@ -1,6 +1,5 @@
 chown -R vscode:vscode /workspaces
 chmod +x run_claude.sh
 # su vscode && chmod +x run_claude.sh  && mkdir ~/.claude-code-router/   && bash
-
 su - vscode -c "mkdir ~/.claude-code-router/"
 su vscode
